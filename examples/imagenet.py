7e2defedd574feeacfc1072f3b97fe3ed09e4938
Matthias Hermann <mhermann@htwg-konstanz.de>
2020-05-25 09:07:36 +0200

    Update imagenet.py

--- examples/imagenet.py
+++ examples/imagenet.py
@@ -14,7 +14,7 @@ dlv = torch.utils.data.DataLoader(imagenetv, num_workers=2, batch_size=20)
 
 model = FasterICA(25, loss="logcosh", optimistic_whitening_rate=0.2, whitening_strategy="GHA")
 
-print("Fitting data shape", f"{len(imagenet)}x{imagenet[0][0].shape[1]}", "on", ica.device)
+print("Fitting data shape", f"{len(imagenet)}x{len(imagenet[0][0])}", "on", ica.device)
 model.fit(dl, 30 , X_val=dlv)
 
 show_filters_color(model.mixing_matrix.T)
